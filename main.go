9df69b6319d9954a8380d5f9c6c7870abcb4ef85
gideonbruce <bruceotieno25@gmail.com>
2024-08-21 15:13:10 +0300

    godotenv

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-func main()
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	godotenv.Load()
+ 
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	fmt.Println("Port", portString)
+}
