e163bdab1ed09d011f4e1c4ad608fc82f7c33e12
gideonotieno <gideonotieno99@gmail.com>
2024-08-30 21:58:23 +0300

    handler-user

--- handler_user.go
+++ handler_user.go
@@ -3,5 +3,16 @@ package main
 import "net/http"
 
 func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `name`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON:", err))
+	}
+
 	respondWithJson(w, 200, struct{}{})
 }
